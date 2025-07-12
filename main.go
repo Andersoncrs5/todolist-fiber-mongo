0752eb866b52f081f7ca6146556c66a43966d53e
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-12 14:25:42 -0300

    Set main.go

--- main.go
+++ main.go
@@ -2,15 +2,67 @@ package main
 
 import (
     "log"
-    "github.com/gofiber/fiber/v3"
+	"os"
+	"os/signal"
+
+	"syscall"
+	"todolist/config"
+	"todolist/handlers"
+	"todolist/repositories"
+	"todolist/services"
+
+    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
-    app := fiber.New()
+	config.ConnectDB()
+	defer config.CloseDB()
 
-    app.Get("/", func(c fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
-    })
+	app := fiber.New()
 
-    log.Fatal(app.Listen(":3000"))
+	app.Use(cors.New())    
+	app.Use(logger.New())  
+	app.Use(recover.New()) 
+
+	todoRepo := repositories.NewTodoRepository(config.MongoDatabase)
+	todoService := services.NewTodoService(todoRepo)
+	todoHandler := handlers.NewTodoHandler(todoService)
+
+	api := app.Group("/api/v1/todos")
+
+	api.Post("/", todoHandler.CreateTodo)
+	api.Get("/", todoHandler.GetAllTodos)
+	api.Get("/:id", todoHandler.GetTodoByID)
+	api.Put("/:id", todoHandler.UpdateTodo)
+	api.Delete("/:id", todoHandler.DeleteTodo)
+	api.Patch("/:id/toggle", todoHandler.ToggleTodoComplete) 
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"status":  "ok",
+			"message": "API is running!",
+		})
+	})
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		_ = <-c
+		log.Println("Shutting down server...")
+		_ = app.Shutdown() 
+	}()
+
+	port := "3000" 
+	
+	log.Printf("Server is starting on port %s", port)
+	if err := app.Listen(":" + port); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+
+	log.Println("Server gracefully stopped.")
 }
